e24965c2528a915e69c1e83d5b1dd7af774d0261
edwinbr <edwinbr@isa.net.ph>
2025-11-13 10:30:13 +0800

    layout basics

--- composeApp/src/commonMain/kotlin/org/kot/cmpcourse/App.kt
+++ composeApp/src/commonMain/kotlin/org/kot/cmpcourse/App.kt
@@ -3,8 +3,11 @@ package org.kot.cmpcourse
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.safeContentPadding
@@ -24,10 +27,46 @@ import cmpcourse.composeapp.generated.resources.compose_multiplatform
 @Composable
 @Preview
 fun App() {
-    Box(modifier = Modifier.fillMaxSize().background(Color.LightGray)) {
+//    Box(modifier = Modifier.fillMaxSize().background(Color.LightGray)) {
+//        Text(
+//            text = "Hello World 1",
+//            modifier = Modifier.align(Alignment.Center)
+//            )
+//    }
+//    Column(
+//        modifier = Modifier.fillMaxSize().background(Color.White),
+//        horizontalAlignment = Alignment.CenterHorizontally,
+//        verticalArrangement = Arrangement.Center
+//    ){
+//        Text(
+//            text = "My Name is json"
+//        )
+//        Text(
+//            text = "My age is 30"
+//        )
+//    }
+    Row(
+        modifier = Modifier.fillMaxSize().background(Color.White),
+        horizontalArrangement = Arrangement.SpaceAround,
+        verticalAlignment =  Alignment.CenterVertically
+    ) {
         Text(
-            text = "Hello World",
-            modifier = Modifier.align(Alignment.Center)
-            )
+            text = "My Name is json"
+        )
+        Text(
+            text = "Text"
+        )
+        Text(
+            text = "My age is 30",
+            modifier = Modifier.clickable{
+                println("Text Clicked!!")
+            }
+        )
+
+        Button(onClick = {
+            println("Button Clicked!!")
+        }){
+            Text("Click")
+        }
     }
 }
